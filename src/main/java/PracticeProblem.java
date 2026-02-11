9dc23144c1d8db81adca9b128dca724b15d9c283
AlexanderAuton <350319867@tdsb.ca>
2026-02-11 16:38:20 +0000

    Work

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -10,22 +10,41 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		int number = 2;
+		int numer = 5;
+		int numr = 3;
+		System.out.println(number + numr + numer);
+		System.out.println(numer * numr * number);
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int number = 10;
+		int numer = 3;
+		System.out.println(10/3);
+		System.out.println(10%3);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		double number = 5.3;
+		double numer = 5.2;
+		System.out.println(number/numer);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		int number = 2;
+		int numer = 3;
+		int numr = 4;
+		System.out.println((number + numer)* numr);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		int number = 5;
+		double numer = 3.5;
+		System.out.println(number+numer);
 	}
 
 }
